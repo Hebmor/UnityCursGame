c1865afe99def507597888a451f1544f02c7e014
DepressingUtopian <DepressingUtopian@gmail.com>
2019-05-05 23:02:13 +0700

    Add Simple Animation NPC
    Создан базовый проект в Unity и настроена простая анимация персонажа на сцене.

---
+++ MobileCursRPG/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 0;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f)); // Translate(x,y,z)
+            //anim.SetBool("IsNewAction", true);
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f)); // Translate(x,y,z)
+           // anim.SetBool("IsNewAction", true);
+        }
+
+        //anim.SetBool("IsNewAction", false);
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+    }
+}
