49b61a2f9c8e5e06dfdfcf40077e7fc0f1e74c79
DepressingUtopian <DepressingUtopian@gmail.com>
2019-05-06 19:59:41 +0700

    Add Camera Move
    - Добавлена привязка камеры к персонажу

---
+++ MobileCursRPG/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject followTarget; //Отслеживаемый камерой объект
+    private Vector3 targetPos;  
+    public float camera_moveSpeed; // > 0 тогда будет двигаться
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Получаем координаты объекта который отслеживает камера и присваемаем эти координаты камере
+        targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position,targetPos, camera_moveSpeed * Time.deltaTime);
+    }
+}
