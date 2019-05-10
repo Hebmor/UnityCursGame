c39c8b1dc26434497685cc2b2b7b5f9df699efe8
Evgeniy <5595gecka@gmail.com>
2019-05-10 21:07:53 +0700

    Add portal script

---
+++ MobileCursRPG/Assets/Scripts/Portal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject tp;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            collision.transform.localPosition = new Vector3(tp.transform.localPosition.x, tp.transform.localPosition.y - 0.33f, tp.transform.localPosition.z - 10);
+        }
+    }
+}
