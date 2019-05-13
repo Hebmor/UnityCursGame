79f84886de6fb3a3867437d0bbcea8ded5a81ce4
Evgeniy <5595gecka@gmail.com>
2019-05-13 23:50:56 +0700

    Add press F to pay respect

--- MobileCursRPG/Assets/Scripts/Portal.cs
+++ MobileCursRPG/Assets/Scripts/Portal.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     public GameObject tp;
+    public GameObject text_press;
 
     // Start is called before the first frame update
     void Start()
-    { 
-        
+    {
+        text_press.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,9 +19,21 @@ public class Portal : MonoBehaviour
         
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //button.enable
+        text_press.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //button.disable
+        text_press.SetActive(false);
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.F))
         {
             collision.transform.localPosition = new Vector3(tp.transform.localPosition.x, tp.transform.localPosition.y - 0.33f, tp.transform.localPosition.z - 10);
         }
