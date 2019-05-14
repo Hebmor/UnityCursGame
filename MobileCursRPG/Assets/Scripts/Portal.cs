e52b031b03e69bdab8627f76489e16677f8ebec0
Aleksey Anisimov <aleksejj.anisimov@gmail.com>
2019-05-15 05:39:02 +0700

    Add simple quest

--- MobileCursRPG/Assets/Scripts/Portal.cs
+++ MobileCursRPG/Assets/Scripts/Portal.cs
@@ -1,19 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
     public int keyID = 0;
-    public GameObject tp;
-    public GameObject text_press;
+    public string nextScene;
     public GameObject text_not;
-    public GameObject playerCamera;
     private float delayMes = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        text_press.SetActive(false);
+        text_not.SetActive(false);
     }
 
     // Update is called once per frame
@@ -30,31 +29,18 @@ public class Portal : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //button.enable
-        text_press.SetActive(true);
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        //button.disable
-        text_press.SetActive(false);
-    }
-
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
             if (collision.GetComponent<PlayerController>().pickedKeys.Contains(keyID))
             {
-                collision.transform.localPosition = new Vector3(tp.transform.localPosition.x, tp.transform.localPosition.y - 0.33f, tp.transform.localPosition.z - 10);
-                playerCamera.transform.position = new Vector3(collision.transform.position.x, collision.transform.position.y, playerCamera.transform.position.z);
+                SceneManager.LoadScene(nextScene);
             }
             else
             {
                 text_not.SetActive(true);
-                delayMes = 2f;
+                delayMes = 3f;
             }
         }
     }
